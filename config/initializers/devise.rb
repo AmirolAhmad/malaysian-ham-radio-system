f16ace4497a56b768c5dc88bb5a2e63a0a6d9099
AmirolAhmad <dev.amirol@gmail.com>
2018-11-30 14:29:20 +0800

    added user devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -145,7 +145,7 @@ Devise.setup do |config|
   config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
-  config.confirmation_keys = [:username]
+  config.confirmation_keys = [:username || :client_number]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
@@ -204,7 +204,7 @@ Devise.setup do |config|
   # ==> Configuration for :recoverable
   #
   # Defines which key will be used when recovering the password for an account
-  config.reset_password_keys = [:username]
+  config.reset_password_keys = [:username || :client_number]
 
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  # for user
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    unlocks: 'users/unlocks'
+  }
+  devise_scope :user do
+    get '/login' => 'users/sessions#new'
+    get '/register' => 'users/registrations#new'
+    get '/settings' => 'users/registrations#edit'
+    root 'users/sessions#new'
+  end
+
+  # for admin
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
     registrations: 'admins/registrations',
@@ -6,5 +22,7 @@ Rails.application.routes.draw do
     passwords: 'admins/passwords',
     unlocks: 'admins/unlocks'
   }
+  
+  # standard
   root 'home#index'
 end
